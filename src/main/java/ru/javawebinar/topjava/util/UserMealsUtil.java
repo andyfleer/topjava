7c22803bf845d13ebd2abc8dd5e6144aea00c8d2
andyfleer <r-enter@ya,ru>
2018-06-26 00:34:09 +0300

    Prepare to HW0. Two algorithms in two methods.

---
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -0,0 +1,85 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.model.UserMealWithExceed;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UserMealsUtil {
+    public static void main(String[] args) {
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+        );
+
+        /*
+         * two algorithms here
+         */
+
+
+      getFilteredWithExceeded(mealList, LocalTime.of(12, 0), LocalTime.of(22,0), 2000);
+//        getFilteredWithExceeded_v2(mealList, LocalTime.of(7, 0), LocalTime.of(22,0), 2000);
+
+
+    }
+
+
+    public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        /*
+         *   BenchMark.ExecutionPlan.benchGetFilteredWithExceededByStream_v01             5  avgt   20   1771,896 ±  16,624  ns/op
+         *   BenchMark.ExecutionPlan.benchGetFilteredWithExceededByStream_v01            10  avgt   20   3910,266 ± 139,203  ns/op
+         *   BenchMark.ExecutionPlan.benchGetFilteredWithExceededByStream_v01            30  avgt   20  11908,832 ± 220,498  ns/op
+         */
+        List<UserMealWithExceed> resultList = new ArrayList<>();
+
+        final int[] sum = {0};
+
+        Map<LocalDate, List<UserMeal>> myMap = mealList.stream()
+                .collect(Collectors.groupingBy(UserMeal::getDate));
+
+        for (Map.Entry<LocalDate, List<UserMeal>> entry :
+                myMap.entrySet()) {
+
+            sum[0] = entry.getValue().stream().map(UserMeal::getCalories).mapToInt(i -> i).sum();
+
+            entry.getValue().stream()
+                    .filter(e -> e.getDateTime().toLocalTime().isAfter(startTime) && e.getDateTime().toLocalTime().isBefore(endTime))
+                    .forEach((e) -> resultList.add(new UserMealWithExceed(e.getDateTime(), e.getDescription(),
+                            e.getCalories(), sum[0] > caloriesPerDay)));
+        }
+
+        return resultList;
+    }
+
+    public static List<UserMealWithExceed> getFilteredWithExceeded_v2(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        /*
+         *     BenchMark.ExecutionPlan.benchGetFilteredWithExceededByStream_v2              5  avgt   20   3223,371 ±  92,552  ns/op
+         *     BenchMark.ExecutionPlan.benchGetFilteredWithExceededByStream_v2             10  avgt   20   6758,335 ±  56,971  ns/op
+         *     BenchMark.ExecutionPlan.benchGetFilteredWithExceededByStream_v2             30  avgt   20  21094,463 ± 101,690  ns/op
+         */
+        List<UserMealWithExceed> resultList = new ArrayList<>();
+
+
+        mealList.stream()
+                .collect(Collectors.groupingBy(UserMeal::getDate)).forEach((key, value) -> value.stream()
+                .filter(e -> (e.getDateTime().toLocalTime().isAfter(startTime) && e.getDateTime().toLocalTime().isBefore(endTime)))
+                .forEach((e) -> resultList.add(new UserMealWithExceed(e.getDateTime(), e.getDescription(), e.getCalories(),
+                        value.stream().map(UserMeal::getCalories).mapToInt(i -> i).sum() > caloriesPerDay))));
+
+        return resultList;
+    }
+}
